f81b324f84d2cab8d2bdfce053f852bee2cc3ade
Joni Hendrickson <contact@joni.site>
2026-04-11 18:47:49 -0700

    Port work stuff

--- modules/work.nix
+++ modules/work.nix
@@ -1,9 +1,29 @@
 {
+  pkgs,
   ...
 }:
 
 {
   programs.git.settings.user.email = "jonathan.hendrickson@bonsairobotics.ai";
+
+  home.packages = with pkgs; [
+    distrobox
+    gnumake
+    vcstool
+    (python3.withPackages (ps: with ps; [ pyyaml ]))
+    awscli
+    podman
+    podman-compose
+    (pkgs.writeShellScriptBin "docker" ''
+      exec podman "$@"
+    '')
+  ];
+
+  # Disable SELinux labeling for containers globally
+  home.file.".config/containers/containers.conf".text = ''
+    [containers]
+    label = false
+  '';
 
   dconf.settings."org/gnome/shell" = {
     favorite-apps = [
