0c3d9248eda5790d3505318af883ce34e9fd6757
Joni Hendrickson <contact@joni.site>
2026-04-24 09:47:54 -0700

    feat(server): add horseradish to syncthing config

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -14,11 +14,14 @@
           introducer = true;
         };
 
+        # Laptop
+        saffron.id = "T2F7ICT-EMNBQH6-TBDQ4DE-7X7J57J-QCGWIS2-VXBN4HB-LRGNZUZ-AFI5IQF";
+
         # DigitalOcean server
         cayenne.id = "WWUKSOV-7RTMEK5-464TFFY-JHMUAPJ-EOO7I5I-L4NB4E6-UHB5357-SP636QU";
 
-        # Laptop
-        saffron.id = "T2F7ICT-EMNBQH6-TBDQ4DE-7X7J57J-QCGWIS2-VXBN4HB-LRGNZUZ-AFI5IQF";
+        # Hetzner server
+        horseradish.id = "PKN3I6Q-MPHESJ5-CTBBLUV-TAEN5KK-5UA27JE-26EUKCL-CVSP74A-RG237QD";
       };
       folders."~/0everything" = {
         id = "0everything";
@@ -31,6 +34,7 @@
           "ginger"
           "cayenne"
           "saffron"
+          "horseradish"
         ];
       };
     };
